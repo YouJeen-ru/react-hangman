2b832d80983a22faf2a219326371f9bc343718ca
YouJeen-ru <1.rouch@mail.ru>
2020-10-08 14:41:38 +0300

    Updates

---
+++ precache-manifest.7f4fcf7a1dd193b2c651108449a32fe1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "add0acc464f91ad1becd07f0d93fa564",
+    "url": "/react-hangman/index.html"
+  },
+  {
+    "revision": "882cdab16182bfb3e0b3",
+    "url": "/react-hangman/static/css/main.0998170a.chunk.css"
+  },
+  {
+    "revision": "f3293ff065b04177a034",
+    "url": "/react-hangman/static/js/2.0e429a6a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hangman/static/js/2.0e429a6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "882cdab16182bfb3e0b3",
+    "url": "/react-hangman/static/js/main.241fa3e4.chunk.js"
+  },
+  {
+    "revision": "ace1c5c5b54d698d81e5",
+    "url": "/react-hangman/static/js/runtime-main.a7ea5c77.js"
+  }
+]);
